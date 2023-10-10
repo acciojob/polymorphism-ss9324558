71c6f4841cdfea7c4dc3f03be4e1c0dbb662caab
ss9324558 <ss9324558@gmail.com>
2023-10-10 06:18:34 +0530

    try2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,19 +6,19 @@ public class Main {
     public static class Product{
         public int product(int x, int y) {return x*y;}
         public int product(int x, int y, int z) {return x*y*z;}
-        public double product(double x, double y) {return ((double)(x*y));}
+        public double product(double x, double y) {return (x*y);}
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         Product p=new Product();
-        Scanner s=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
         int y=sc.nextInt();
         int z=sc.nextInt();
         double a=sc.nextDouble();
         double b=sc.nextDouble();
-        p.product(x,y);
-        p.product(x,y,z);
-        p.product(a,b);
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product(a,b));
     }
 
 }
