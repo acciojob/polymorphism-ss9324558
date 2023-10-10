c3164c95880a178d36e6e027244678b4a2e4b7c8
ss9324558 <ss9324558@gmail.com>
2023-10-10 06:15:44 +0530

    try2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,24 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
     public static class Product{
-        public int product(int x, int y) {return 0;}
-        public int product(int x, int y, int z) {return 0;}
-        public double product(double x, double y) {return 0;}
+        public int product(int x, int y) {return x*y;}
+        public int product(int x, int y, int z) {return x*y*z;}
+        public double product(double x, double y) {return ((double)(x*y));}
     }
     public static void main(String args[]){
         Product p=new Product();
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(1,2);
+        Scanner s=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
     }
 
 }
