b76561d4cd62fb7bc37db0474fa00a3e9caccabf
ss9324558 <ss9324558@gmail.com>
2023-10-10 06:05:17 +0530

    try1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {return 0;}
         public int product(int x, int y, int z) {return 0;}
         public double product(double x, double y) {return 0;}
